87ec78f6766aa566f0381fbb9b6895ee9524bcfb
System-03 <puttursunil@gmail.com>
2016-09-08 15:03:19 +0530

    trying git...

---
+++ app/src/main/java/com/example/system_03/drgarmentandwater/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.system_03.drgarmentandwater;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by System-03 on 9/8/2016.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
